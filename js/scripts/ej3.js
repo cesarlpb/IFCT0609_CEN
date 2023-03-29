adcdae35a7f9308ad0af63022a6033609009afdc
César <cesarlpb@gmail.com>
2023-03-29 13:41:08 +0200

    Ej 3 - Suma, resta, multiplicación y división

--- js/scripts/ej3.js
+++ js/scripts/ej3.js
@@ -1,1 +1,28 @@
-console.log("ej 3")
+let form2 = document.getElementById("form2");
+function handleForm(event) { event.preventDefault(); } 
+form2.addEventListener('submit', handleForm);
+
+function suma(){
+    let numero3 = document.getElementById("numero3").valueAsNumber
+    let numero4 = document.getElementById("numero4").valueAsNumber
+    let resultado = numero3 + numero4
+    document.getElementById("resultado").innerText = "Suma: " + resultado.toLocaleString()
+}
+function resta(){
+    let numero3 = document.getElementById("numero3").valueAsNumber
+    let numero4 = document.getElementById("numero4").valueAsNumber
+    let resultado = numero3 - numero4
+    document.getElementById("resultado").innerText = "Resta: " + resultado.toLocaleString()
+}
+function prod(){
+    let numero3 = document.getElementById("numero3").valueAsNumber
+    let numero4 = document.getElementById("numero4").valueAsNumber
+    let resultado = numero3 * numero4
+    document.getElementById("resultado").innerText = "Producto: " + resultado.toLocaleString()
+}
+function divi(){
+    let numero3 = document.getElementById("numero3").valueAsNumber
+    let numero4 = document.getElementById("numero4").valueAsNumber
+    let resultado = numero3 / numero4
+    document.getElementById("resultado").innerText = "División: " + resultado.toLocaleString()
+}
