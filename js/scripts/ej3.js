e2847f24a3ed02af42836807bfcdbbcc6daeac0b
César <cesarlpb@gmail.com>
2023-03-29 14:01:33 +0200

    Ej3 - Calc con mas operaciones

--- js/scripts/ej3.js
+++ js/scripts/ej3.js
@@ -7,22 +7,38 @@ function suma(){
     let numero4 = document.getElementById("numero4").valueAsNumber
     let resultado = numero3 + numero4
     document.getElementById("resultado").innerText = "Suma: " + resultado.toLocaleString()
+    return resultado
 }
 function resta(){
     let numero3 = document.getElementById("numero3").valueAsNumber
     let numero4 = document.getElementById("numero4").valueAsNumber
     let resultado = numero3 - numero4
     document.getElementById("resultado").innerText = "Resta: " + resultado.toLocaleString()
+    return resultado
 }
 function prod(){
     let numero3 = document.getElementById("numero3").valueAsNumber
     let numero4 = document.getElementById("numero4").valueAsNumber
     let resultado = numero3 * numero4
     document.getElementById("resultado").innerText = "Producto: " + resultado.toLocaleString()
+    return resultado
 }
 function divi(){
     let numero3 = document.getElementById("numero3").valueAsNumber
     let numero4 = document.getElementById("numero4").valueAsNumber
     let resultado = numero3 / numero4
     document.getElementById("resultado").innerText = "División: " + resultado.toLocaleString()
+    return resultado
+}
+function todo(){
+    let resultado1 = suma()
+    let resultado2 = resta()
+    let resultado3 = prod()
+    let resultado4 = divi()
+    let resultado = ""
+    resultado += "Suma: " + resultado1.toLocaleString()
+    resultado += "\nResta: " + resultado2.toLocaleString()
+    resultado += "\nProducto: " + resultado3.toLocaleString()
+    resultado += "\nDivisión: " + resultado4.toLocaleString()
+    document.getElementById("resultado").innerText = resultado
 }
