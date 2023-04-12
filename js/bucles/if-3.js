de71a4e8c486462d915440e2bd74c853d046cf33
César <cesarlpb@gmail.com>
2023-04-12 12:29:13 +0200

    Ej 3 - raíz cuadrada

--- js/bucles/if-3.js
+++ js/bucles/if-3.js
@@ -1,1 +1,12 @@
 //3. Vamos a calcular la raíz cuadrada de un número. Verificamos que no nos pasen un número negativo o algo que no sea un número.
+
+function raizCuadrada(){
+    let numero = document.getElementById("ej3").valueAsNumber
+    if ( numero < 0 ){
+        resultado = "Debes introducir un número positivo."
+    } else {
+        resultado = Math.sqrt(numero)
+    }
+    let elemento = document.getElementById("resultado7")
+    elemento.innerText = resultado
+}
