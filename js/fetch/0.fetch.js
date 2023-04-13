42fede03bd972601a04d4a720c5917d586315db2
César <cesarlpb@gmail.com>
2023-04-13 08:49:29 +0200

    fn - editarTodo

--- js/fetch/0.fetch.js
+++ js/fetch/0.fetch.js
@@ -2,10 +2,10 @@
 // Usamos este API: https://jsonplaceholder.typicode.com/guide/
 
 // GET
-// petición para "conseguir" un recurso (objeto con datos -> enlaces, img, JSON con objetos, etc.)
+// petición para "obtener" o "conseguir" un recurso (objeto con datos -> enlaces, img, JSON con objetos, etc.)
 
 // GET de toda la lista
-// Colocamos el código en una función para llamarla haciendo clic en el botón
+// Pedir. Colocamos el código en una función para llamarla haciendo clic en el botón
 function pedirListaTodos(){
     // GET de lista
     // Creamos una variable para guardar la respuesta de este endpoint
@@ -18,6 +18,7 @@ function pedirListaTodos(){
 }
 
 // GET de un recurso
+// Pedir un recurso.
 
 function pedirUnTodo(){
     let todoId = document.getElementById("todoId").value
@@ -39,7 +40,7 @@ function pedirUnTodo(){
 }
 
 // POST
-// Enviar un objeto y recibir una respuesta
+// Crear. Acción de "enviar" o "subir" un objeto y recibir una respuesta
 // - Crear un recurso nuevo (una entrada en un blog, un post, etc.)
 // - Editar un recurso
 
@@ -63,4 +64,30 @@ function crearTodo(){
       })
         .then((response) => response.json())
         .then((json) => elemento.innerText = JSON.stringify(json));
+}
+
+// PUT
+// Editar. Acción de "colocar" o "poner" valores nuevos en un recurso existente. 
+function editarTodo(){
+  let todoId = document.getElementById("todoId2").value
+  let titulo = document.getElementById("titulo2").value
+  let estaCompletado = document.getElementById("estaCompletado2").checked // checkbox
+  let userId = document.getElementById("userId2").valueAsNumber
+
+  let elemento = document.getElementById("editarTodo");
+
+  fetch('https://jsonplaceholder.typicode.com/todos/' + todoId, {
+    method: 'PUT',
+    body: JSON.stringify({
+      id: todoId,
+      title: titulo,
+      completed: estaCompletado,
+      userId: userId,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => elemento.innerText = JSON.stringify(json));
 }
