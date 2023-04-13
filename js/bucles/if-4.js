6fee045da617443798b580f6e4378b362d6db3be
César <cesarlpb@gmail.com>
2023-04-13 11:58:16 +0200

    Ej 4 - comprobarHorarioLaboral

--- js/bucles/if-4.js
+++ js/bucles/if-4.js
@@ -1,1 +1,21 @@
 //4. Mirando la hora actual, vamos a comprobar si es horario laboral. Horario laboral: 9 AM a 6 PM.
+
+function comprobarHorarioLaboral(){
+    
+    // Mirar que hora es:
+    let date = new Date()
+    let horaActual = date.getHours()
+    
+    // Comprobar si es horario laboral
+    // Horario laboral es entre las 9 y las 18 h
+    let mensaje = ""
+
+    if( horaActual >= 9 && horaActual <= 18 ){
+        mensaje = "Es horario laboral"
+    } else {
+        mensaje = "No es horario laboral :)"
+    }
+
+    let elemento = document.getElementById("resultado8")
+    elemento.innerText = mensaje
+}
