3d30713ae52692ae4e7ce73a8a6dfce3c98426e0
César <cesarlpb@gmail.com>
2023-04-14 09:18:18 +0200

    Ejemplos y ejercicio con switch

--- js/bucles/switch.js
+++ js/bucles/switch.js
@@ -25,6 +25,34 @@ switch (fecha.getDay()) {
       dia = "Sábado";
   }
 
-  let fechaCorta = fecha.toLocaleDateString() // dd/mm/aaaa
-  let mensaje = "Hoy es " + dia.toLowerCase() + ", " + fechaCorta;
-  console.log(mensaje)
+let fechaCorta = fecha.toLocaleDateString() // dd/mm/aaaa
+let mensaje = "Hoy es " + dia.toLowerCase() + ", " + fechaCorta;
+console.log(mensaje)
+
+
+switch(fecha.getDay()){
+    case 6:
+        mensaje = "Hoy es sábado!"
+        break;
+    case 0:
+        mensaje = "Hoy es domingo!!"
+        break;
+    default:
+        mensaje = "Es día laboral"
+}
+console.log(mensaje)
+
+switch(fecha.getDay()){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        mensaje = "Hoy toca programar!"
+        break;
+    default:
+        mensaje = "Hoy toca descansar!"
+}
+console.log(mensaje)
+
+// Ejercicio: hacer un switch que me escriba el mes actual a partir de getMonth()
