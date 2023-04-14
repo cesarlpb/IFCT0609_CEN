96b1b49bae8e4238e9f3adb46c55545c55771b2c
César <cesarlpb@gmail.com>
2023-04-14 10:01:35 +0200

    Ejercicio - meses con switch

--- js/bucles/switch.js
+++ js/bucles/switch.js
@@ -56,3 +56,44 @@ switch(fecha.getDay()){
 console.log(mensaje)
 
 // Ejercicio: hacer un switch que me escriba el mes actual a partir de getMonth()
+
+let mes = ""
+
+switch(fecha.getMonth()){
+    case 0:
+        mes = "Enero"
+        break;
+    case 1:
+        mes = "Febrero"
+        break;
+    case 2:
+        mes = "Marzo"
+        break;
+    case 3:
+        mes = "Abril"
+        break;
+    case 4:
+        mes = "Mayo"
+        break;
+    case 5:
+        mensaje = "Junio"
+        break;
+    case 6:
+        mes = "Julio"
+        break;
+    case 7:
+        mes = "Agosto"
+        break;
+    case 8:
+        mes = "Septiembre"
+        break;
+    case 9:
+        mes = "Octubre"
+        break;
+    case 10:
+        mes = "Noviembre"
+        break;
+    case 11:
+        mes = "Diciembre"
+}
+console.log(mes)
