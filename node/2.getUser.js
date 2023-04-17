a5c1ec6af76b394c664c568b8542b465b7228a88
César <cesarlpb@gmail.com>
2023-04-17 12:09:28 +0200

    GET de un user en JSON

---
+++ node/2.getUser.js
@@ -0,0 +1,14 @@
+var http = require('http');
+const port = 8080;
+
+console.log("App disponible en: http://localhost:" + port);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  const user =   {
+    id: 1, 
+    name: 'John Doe', 
+    email: 'johndoe@example.com'
+};
+  res.end(JSON.stringify(user));
+}).listen(port);
