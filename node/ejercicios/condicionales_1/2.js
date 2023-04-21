62ca6c151abbb7cc7cbfcd1e04459be567c61668
César <cesarlpb@gmail.com>
2023-04-21 10:05:01 +0200

    Ej 2 - número positivo o negativo

---
+++ node/ejercicios/condicionales_1/2.js
@@ -0,0 +1,17 @@
+/**
+ * Un programa que pida un número y diga si es positivo o negativo. El cero se considera positivo
+ * 
+ * Si num1 es 2 debe decir que num1 es positivo
+ * 
+ */
+
+let num = Number(prompt("Introduce un número:"))
+if(num < 0){
+    // estrictamente menor que cero
+    console.log(`${num} es negativo`)
+} else {
+    // igual o mayor que cero
+    console.log(`${num} es positivo`)
+}
+
+// No estamos comprobando si nos pasan otro tipo de dato que al convertir con Number daría NaN
