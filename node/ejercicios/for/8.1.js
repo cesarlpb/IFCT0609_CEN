a8c5ce1b6fbc7fcdaf70cf29d6e1d33eb9d72b31
César <cesarlpb@gmail.com>
2023-04-26 13:04:07 +0200

    8.1 Usamos repeat() en fn trianguloIzq()

--- node/ejercicios/for/8.1.js
+++ node/ejercicios/for/8.1.js
@@ -30,16 +30,17 @@ etc
 
 // Modificamos el siguiente código:
 let num = prompt("Introduce un número entero:")
-let mensaje = trianguloInvDer(num) // global
+let mensaje = trianguloIzq(num) // global
 console.log(mensaje) // en alert() la alineación es distinta pero el resultado es correcto
 
 function trianguloIzq(num){
     let mensaje = "" // local
     if(num == parseInt(num) && Number(num) > 0){
         for(let i = 0; i<=num; i++){
-            for(let j = 0; j<i; j++){
-                mensaje += "*"
-            }
+            // for(let j = 0; j<i; j++){
+            //     mensaje += "*"
+            // }
+            mensaje += "*".repeat(i)
             mensaje += "\n"
         }
     }else{
