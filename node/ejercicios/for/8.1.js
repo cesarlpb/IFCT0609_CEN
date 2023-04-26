b404e3c33b18d1976dc6949eaf98703b43cc50e5
César <cesarlpb@gmail.com>
2023-04-26 12:45:16 +0200

    Ej 8.1 - fn de triángulo a la derecha

--- node/ejercicios/for/8.1.js
+++ node/ejercicios/for/8.1.js
@@ -1,4 +1,12 @@
 /**
+ * Triángulo a la derecha
+ * espacios = num - i -> "  " + "*"
+    *
+   **
+  ***
+ ****
+*****
+
  * Triángulo invertido a la izq
 
 *****
@@ -7,12 +15,7 @@
 **
 *
 
-Triángulo a la derecha
-    *
-   **
-  ***
- ****
-*****
+
 
 Triángulo a la derecha invertido
 etc
@@ -22,13 +25,33 @@ etc
 
 // Modificamos el siguiente código:
 let num = prompt("Introduce un número entero:")
-let mensaje = trianguloIzq(num) // global
-alert(mensaje)
+let mensaje = trianguloDer(num) // global
+console.log(mensaje) // en alert() la alineación es distinta pero el resultado es correcto
 
 function trianguloIzq(num){
     let mensaje = "" // local
     if(num == parseInt(num) && Number(num) > 0){
         for(let i = 0; i<=num; i++){
+            for(let j = 0; j<i; j++){
+                mensaje += "*"
+            }
+            mensaje += "\n"
+        }
+    }else{
+        mensaje = "Debes introducir un número positivo"
+    }
+    return mensaje
+}
+
+function trianguloDer(num){
+    let mensaje = "" // local
+    let espacios = 0
+    if(num == parseInt(num) && Number(num) > 0){
+        for(let i = 0; i<=num; i++){
+            espacios = num - i
+            for(let k = 0; k < espacios; k++){
+                mensaje += " "
+            }
             for(let j = 0; j<i; j++){
                 mensaje += "*"
             }
