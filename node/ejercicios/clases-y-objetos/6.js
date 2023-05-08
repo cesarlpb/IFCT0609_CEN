b244e73897221563786740b659363f1eba8c4e95
César <cesarlpb@gmail.com>
2023-05-08 12:31:50 +0200

    Ej 6 - código inicial

---
+++ node/ejercicios/clases-y-objetos/6.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Crea una clase que lamaremos Bus. Sus atributos serán:
+
+* capacidad: número máximo de pasajeros             -> Number, int -> const
+* pasajeros: número de pasajeros (inicialmente 0)   -> Number, int -> inicia a 0
+* conductor: objeto conductor.                        -> Conductor es objeto de clase Conductor
+
+Sus métodos (de Bus)
+
+* subir(pasajeros): aumenta el numero de pasajeros      -> admite positivos
+* bajar(pasajeros): disminuye el número de pasajeros    -> admite positivos y no pueden bajar más de los pasajeros actuales
+* conductor: asigna un objeto conductor.                -> objeto Conductor
+
+El objeto conductor es de una clase (Conductor) cuyos atributos son:
+
+* nombre: nombre del conductor                      -> se pasa el param en creación del objeto
+* licencia: un número que identifica al condcutor.  -> Number, int o podría ser String
+Al crear el objeto se asigna también el conductor   -> no inicia vacío
+
+Restricciones para que tenga sentido el ejercicio:
+No pueden subir más pasajeros que los máximos admitidos y no pueden bajar más de los que hay.
+
+ */
+
+class Conductor{
+  
+}
+class Bus{
+    // conductor
+}
