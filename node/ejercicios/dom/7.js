d7be5c267e147f9a4b3df7c57c09bbdb4de8fbc1
César <cesarlpb@gmail.com>
2023-05-10 13:13:03 +0200

    Ej 7.Toggle <h1> entre activo/no activo con click

---
+++ node/ejercicios/dom/7.js
@@ -0,0 +1,3 @@
+/*
+El script que tienes que crear en la página modelo debe cambiar la clase del elemento que contiene al encabezado de la página. Al pulsar sobre el titular se añadirá o se eliminarás (toggle) la clase de nombre activo
+*/
