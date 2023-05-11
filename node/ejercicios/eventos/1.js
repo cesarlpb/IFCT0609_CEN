3991fe7fc576c926ee6fff9f1ee99cc1507b7139
César <cesarlpb@gmail.com>
2023-05-11 10:26:18 +0200

    Ej 1 - click en div que muestra un alert()

---
+++ node/ejercicios/eventos/1.js
@@ -0,0 +1,7 @@
+/*
+En una página web vas a colocar un elemento tipo div con un argumento onclick() para que al pulsar sobre el bloque se abra una ventana alert con el mensaje "Pulsaste sobre el botón". El bloque div deberá tener como tecto "Pulsa aquí".
+*/
+function ej1(){
+    console.log("Click");
+    alert("Hiciste click!")
+}
