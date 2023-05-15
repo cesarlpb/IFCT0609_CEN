f4ce04e71d5797841343ae660f0eaef26f5f24b4
laomara <laomara>
2023-05-15 08:44:40 +0200

    Ej 1 y 2 - método de array map()

---
+++ Node/Ejercicios/Arrays2/1.js
@@ -0,0 +1,5 @@
+/*
+Este ejercicio solo te pide que crees un array de nombre diasSemana para contener 7 elementos. Luego rellena el array con los nombres de los días comenzando por Lunes hasta Domingo.
+*/
+
+let diasSemana = ["lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo"]
