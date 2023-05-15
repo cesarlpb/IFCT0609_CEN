c80df97c0aebcc9e06d6cd331d8ad969d206513d
César <cesarlpb@gmail.com>
2023-05-15 13:11:35 +0200

    Ej 1 - resolvemos ejercicio con métodos de array

--- node/ejercicios/string/1.js
+++ node/ejercicios/string/1.js
@@ -9,7 +9,6 @@ a:1, e:2, i:0, o:2, u:0
 
 function cuentaVocales(string) {
     let vocales = "aeiou"; // podemos usar un string que es un array de caracteres o un array de caracteres -> en ambos se usan índices numéricos
-    let resultado = "";
     let contadorA = 0, contadorE = 0, contadorI = 0, contadorO = 0, contadorU = 0;
     for (let i = 0; i < string.length; i++) {
         let char = string[i].toLowerCase();
@@ -31,4 +30,17 @@ function cuentaVocales(string) {
         }
     }
     return `a:${contadorA}, e:${contadorE}, i:${contadorI}, o:${contadorO}, u:${contadorU}`
+}
+
+function cuentaVocales2(string) {
+    let vocales = "aeiou";
+    let contador = [0, 0, 0, 0, 0];
+    [...string].map(char => {
+        let index = vocales.indexOf(char); // devuelve 0-4 si lo encuentra o -1 si no lo encuentra
+        if (index >= 0) {
+            contador[index]++;
+        }
+    })
+    // TODO: sería interesante plantear esto con un map o con un objeto que agrupe ambos arrays
+    return `a:${contador[0]}, e:${contador[1]}, i:${contador[2]}, o:${contador[3]}, u:${contador[4]}`
 }
