633c4e50981162c83ac01048b7f9a4a89c6fddeb
César <cesarlpb@gmail.com>
2023-05-16 12:55:20 +0200

    Reorganización de archivos de node a ejercios js

---
+++ js/ejercicios/14.Date/1.js
@@ -0,0 +1,8 @@
+/*
+En este ejercicio el usuario te dará su fecha de nacimiento y debes escribir la función que calcula el dia de la semana en que nació. Se supone que el usuario introducirá los datos numéricos correctamente y en una fecha real.
+
+Por ejemplo si la fecha de nacimiento es el 3 de febrero de 1990 (3, 2, 1990) la función debe decirtes que nació en Sábado
+
+Pedimos que escriba el formato de fecha: dd/mm/aaaa
+
+*/
