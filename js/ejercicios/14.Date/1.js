30e2a7b7ca172107bf3d26f8caaa63cb920bdd67
César <cesarlpb@gmail.com>
2023-05-16 13:26:11 +0200

    Ej 1 - Fn para recibir fecha en formato dd/mm/yyyy

--- js/ejercicios/14.Date/1.js
+++ js/ejercicios/14.Date/1.js
@@ -7,9 +7,33 @@ Pedimos que escriba el formato de fecha: dd/mm/aaaa
 
 */
 
-let fechaStr = prompt("Introduce tu fecha de nacimiento en formato mm/dd/aaaa: ")
-let fecha = new Date(fechaStr)
+let fechaStr = prompt("Introduce tu fecha de nacimiento en formato dd/mm/aaaa: ")
+/* Observación:
+Este formato puede ser un poco confuso para hispanohablantes ya que en España y en la mayoría de países de habla hispana se usa el formato dd/mm/aaaa, hay dos opciones para cambiar esto:
+1. Mirar en la documentación como funciona la conversión de string con Date() para ver si es posible cambiar el formato de fecha que espera el constructor
+2. Hacemos una función que nos haga el cambio en el string que pasamos a Date()
+
+dd/mm/aaaa -> mm/dd/aaaa
+
+*/
+let fecha = new Date(cambiarFormato(fechaStr))
 let dia = fecha.getDay() // le pasamos este dato a diasSemana() para que nos devuelva el nombre del día
-console.log(dia)
 let diaSemana = diasSemana(dia)
-alert(`Naciste un ${diaSemana}`)
+if(!diaSemana){
+    alert("Formato de fecha incorrecto")
+  }else{
+    console.log(diaSemana)
+    alert(`Naciste un ${diaSemana}`)
+  }
+
+function cambiarFormato(fechaStr){
+    // Recibimos un string con datos separados por / -> dd/mm/aaaa
+    let arr = fechaStr.split("/")
+    if(arr.length !== 3){
+        console.error("Formato de fecha incorrecto")
+        return ""
+    }else{
+        let [dia, mes, año] = arr;
+        return `${mes}/${dia}/${año}`
+    }
+}
