06ab5c17551f332e8cf447506e00b907a1125ba4
César <cesarlpb@gmail.com>
2023-05-17 13:10:54 +0200

    El 7. Reordeno archivos

--- js/ejercicios/14.Date/7.js
+++ js/ejercicios/14.Date/7.js
@@ -1,31 +1,11 @@
 /*
-Esta funcion debe determinar si el año de una determinada fecha es bisiesto o no. Recuerda un año es bisiesto si es múltiplo de 4, excepto el primer año del siglo (1500, 1600...) que solo lo será es divisible por 400
+Escribe una función que determina cual de las dos horas que se le pasa como argumento es menor.
 
-esBisiesto('1984/1/1') // true
-esBisiesto('1985/1/1') // false
-esBisiesto('1600/1/1') // true
-esBisiesto('1800/1/1') // false
+Se supone que ambos tiempos son horas de un mismo día y la solución se debe hacer usando el objeto Date.
+
+Por ejemplo menorHora("12:20:00", "11:10:00") debiera de decir que la segunda hora es menor que la primera. 
 */
 
-/**
- * Calcula si el año de una fecha es bisiesto
- * @param {string} año yyyy
- * @returns true o false
- */
-function esBisisesto(año_){
-  let f = new Date(`${año_}/1/1`);
-  if(isNaN(f.valueOf())){
-      throw new Error("El año no es válido");
-  }
-  let año = f.getFullYear();
-  if(año % 100 == 0){
-      return año % 400 == 0;
-  }else{
-      return año % 4 == 0;
-  }
+function horaMenor(hora1, hora2){
+    return "string"
 }
-
-document.write("1984 es bisiesto? " + esBisisesto('1984') + "<br/>"); // true
-document.write("1985 es bisiesto? " + esBisisesto('1985') + "<br/>"); // false
-document.write("1600 es bisiesto? " + esBisisesto('1600') + "<br/>"); // true
-document.write("1800 es bisiesto? " + esBisisesto('1800') + "<br/>"); // false
