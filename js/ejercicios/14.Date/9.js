63e52e3f3fab92ac943b72ae7a256aa78ed4328c
César <cesarlpb@gmail.com>
2023-05-17 13:42:04 +0200

    Ej 9 - Calcular el último día del mes (revisar)

---
+++ js/ejercicios/14.Date/9.js
@@ -0,0 +1,34 @@
+/*
+Te propongo que crees una función para determinar el número de días de cada mes, por supuesto sin usar arrays con meses y días, si no usando el objeto Date. La función recibe como argumento una fecha.
+
+Así por ejemplo:
+
+diasMes("2000/2/1"), es el mes de febrero de un año bisiesto, luego dará como resultado 29
+
+diasMes("2023/1/1") dará 31
+*/
+
+/**
+ * Calcula el máximo número de días de un mes a partir de una fecha
+ * @param {string} fecha_ yyyy/mm/dd
+ * @returns {number} Número de días del mes
+ */
+function ultimoDiaDelMes(fecha_){
+    let fecha = new Date(fecha_);
+    if(isNaN(fecha.valueOf())){
+        throw new Error("Fecha no válida");
+    }
+    let dia = fecha.getDate(); 
+    let mes = fecha.getMonth();
+    let nuevaFecha = fecha;
+    let ultimoDia = dia;
+    let contador = 0;
+    while(mes == fecha.getMonth() && !isNaN(nuevaFecha)){
+      nuevaFecha = new MiDate(new Date(fecha_)).sumarDias(contador).fecha; // objeto Date()
+      contador++;
+      ultimoDia++;
+      mes = nuevaFecha.getMonth();
+      console.log(mes, nuevaFecha, fecha.getMonth())
+    }
+    return ultimoDia-1; // discrepancia de 2 días ???
+}
