ec4d4d65c31ab6b6bc6b3703880d0a0d172ab4d5
César <cesarlpb@gmail.com>
2023-05-26 10:05:21 +0200

    Añadimo funciones para escribir teclas

--- js/proyectos/calculadora/calculadora.js
+++ js/proyectos/calculadora/calculadora.js
@@ -1,1 +1,28 @@
-console.log('calculadora.js cargado')
+function escribir(div){
+  console.log(div.innerText)
+  // Escribir en el display con id="resultado"
+  let tecla = div.innerText 
+  let resultado = document.getElementById("resultado")
+  if(resultado.innerText == "0"){
+    resultado.innerText = ""
+  }
+  if(tecla == "," && !resultado.innerText.includes(",")){
+    resultado.innerText += tecla // concatenar la coma
+  }else if(tecla != ","){
+    resultado.innerText += tecla
+  }
+}
+function borrar(){
+  // Borrar el display con id="resultado"
+  document.getElementById("resultado").innerText = "0"
+  console.log("Borrado")
+}
+function cambiarSigno(){
+  console.log("Cambio de signo")
+}
+function porcentaje(){
+  console.log("Porcentaje")
+}
+function calcular(){
+  console.log("Calcular")
+}
