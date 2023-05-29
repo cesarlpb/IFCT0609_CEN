b21e57dd7b0e0dbb4872a9d5eecadccce3d95373
César <cesarlpb@gmail.com>
2023-05-29 11:32:07 +0200

    Fn seccionActual

--- js/proyectos/2.formulario_paginado/formulario.js
+++ js/proyectos/2.formulario_paginado/formulario.js
@@ -1,19 +1,46 @@
-// Habilitar los botones de navegación
-// Cambiar el display de none a block según el click en cada botón
-
 let btnAnterior = document.getElementById('anterior');
 let btnSiguiente = document.getElementById('siguiente');
 let btnEnviar = document.getElementById('enviar');
 
 btnAnterior.addEventListener('click', (e) => { 
   e.preventDefault();
-  console.log("anterior");
+  // Determinar en qué sección estamos o cuál esta activa
+  // Cambiar a la anterior sección
+  let seccionActiva = seccionActual();
+  cambiarSeccion(seccionActiva-1)
+  console.log("anterior", seccionActiva);
 });
 btnSiguiente.addEventListener('click', (e) => { 
   e.preventDefault();
-  console.log("siguiente");
+  // Determinar en qué sección estamos o cuál esta activa
+  // Cambiar a la siguiente sección
+  let seccionActiva = seccionActual();
+  cambiarSeccion(seccionActiva+1)
+  console.log("siguiente", seccionActiva);
 });
 btnEnviar.addEventListener('click', (e) => { 
   e.preventDefault();
-  console.log(enviar); 
+  console.log("enviar"); 
 });
+
+// Funciones
+/**
+ * Calculamos la sección actual: 1, 2, 3 en este caso.
+ * @returns {Number} Número de la sección actual
+ */
+function seccionActual() {
+  let secciones = document.querySelectorAll('section'), seccionActiva = 0;
+  // Observación / TODO: para una lista larga si queremos hacer break habría que usar un bucle for
+  secciones.forEach((seccion, index) => {
+    if (seccion.classList.contains('active')) {
+      seccionActiva = index+1;
+    }
+  });
+  return seccionActiva
+}
+/**
+ * @param {Number} numero de la seccion a la que se quiere cambiar
+ */
+function cambiarSeccion(numero) {
+  // cambia a la sección del param
+}
