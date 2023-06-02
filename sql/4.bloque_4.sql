c6c53eb736f8cde4b9afcf764e4eddc0ff72a791
César <cesarlpb@gmail.com>
2023-06-02 11:59:10 +0200

    Bloque 4 - código inicial

---
+++ sql/4.bloque_4.sql
@@ -0,0 +1,23 @@
+-- 1. Contar el número de usuarios por marca de teléfono
+
+-- 2. Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca LG
+
+-- 3. Listar las diferentes compañias en orden alfabético ascendentemente
+
+-- 4. Calcular la suma de los saldos de los usuarios de la compañia telefónica UNEFON
+
+-- 5. Mostrar el email de los usuarios que usan hotmail
+
+-- 6. Listar los nombres de los usuarios sin saldo o inactivos
+
+-- 7. Listar el login y teléfono de los usuarios con compañia telefónicaIUSACELL o TELCEL
+
+-- 8. Listar las diferentes marcas de celular en orden alfabético ascendentemente
+
+-- 9. Listar las diferentes marcas de celular en orden alfabético aleatorio
+
+-- 10. Listar el login y teléfono de los usuarios con compañia telefónica IUSACELL o UNEFON
+
+-- 11. Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca MOTOROLA o NOKIA
+
+-- 12. Calcular la suma de los saldos de los usuarios de la compañia telefónica TELCEL
