3ba04af3c84de36e3c1d0838eae95721547c13a4
César <cesarlpb@gmail.com>
2023-06-02 10:43:49 +0200

    Bloque 3 - código inicial

---
+++ sql/3.bloque_3.sql
@@ -0,0 +1,23 @@
+-- 1. Listar el login de los usuarios con nivel 1 o 3
+
+-- 2. Listar nombre y teléfono de los usuarios con teléfono que no sea de la marca BLACKBERRY
+
+-- 3. Listar el login de los usuarios con nivel 3
+
+-- 4. Listar el login de los usuarios con nivel 0
+
+-- 5. Listar el login de los usuarios con nivel 1
+
+-- 6. Contar el número de usuarios por sexo
+
+-- 7. Listar el login y teléfono de los usuarios con compañia telefónica AT&T
+
+-- 8. Listar las diferentes compañias en orden alfabético descendentemente
+
+-- 9. Listar el logn de los usuarios inactivos
+
+-- 10. Listar los números de teléfono sin saldo
+
+-- 11. Calcular el saldo mínimo de los usuarios de sexo “Hombre”
+
+-- 12. Listar los números de teléfono con saldo mayor a 300
