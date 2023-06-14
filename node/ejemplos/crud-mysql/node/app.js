88c82a9fc905e5161f843be5af4c83b37662c5e1
César <cesarlpb@gmail.com>
2023-06-14 12:46:36 +0200

    Ej: añadir lista de endpoints en HTML en ruta base

--- node/ejemplos/crud-mysql/node/app.js
+++ node/ejemplos/crud-mysql/node/app.js
@@ -3,4 +3,5 @@ const http = require('http');
 http.createServer(function (req, res) {
   console.log("Consola del servidor")
   res.end("Salida para el navegador") // por defecto es text/plain
+  // TODO: cambiamos el response a HTML -> enlaces de endpoints que vamos a crear
 }).listen(8080, () => { console.log("Servidor en ejecución en http://localhost:8080") })
