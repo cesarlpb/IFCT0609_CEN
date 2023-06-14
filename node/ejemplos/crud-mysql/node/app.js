1368d9f77c383b8ce1854d646f907b4eb50fdc8c
César <cesarlpb@gmail.com>
2023-06-14 13:52:14 +0200

    Ej de respuesta con HTML por defecto y en /albumes

--- node/ejemplos/crud-mysql/node/app.js
+++ node/ejemplos/crud-mysql/node/app.js
@@ -2,6 +2,27 @@ const http = require('http');
 
 http.createServer(function (req, res) {
   console.log("Consola del servidor")
-  res.end("Salida para el navegador") // por defecto es text/plain
-  // TODO: cambiamos el response a HTML -> enlaces de endpoints que vamos a crear
+  // TODO: colocar un switch para manejar las rutas
+  if(req.url == "/albumes"){
+    res.writeHead(200, { 'Content-Type': 'text/html; charset = UTF-8' })
+    res.write("<h1>Albumes</h1>")
+    res.end()
+  }else{
+    res.writeHead(200, { 'Content-Type': 'text/html; charset = UTF-8' })
+    let html = generarHTML()
+    res.write(html)
+    res.end() 
+  }
 }).listen(8080, () => { console.log("Servidor en ejecución en http://localhost:8080") })
+
+function generarHTML() {
+  let arr = ["albumes", "artistas", "posts", "tracks", "usuarios"]
+  let baseUrl = "http://localhost:8080"
+  let html = ""
+  html += "<h1>Endpoints</h1>"
+  for(let i = 0; i<arr.length; i++){
+    let enlace = baseUrl + "/" + arr[i] 
+    html += `<a href=${enlace} target="_blank">${arr[i]}</a><br>`
+  }
+  return html
+}
