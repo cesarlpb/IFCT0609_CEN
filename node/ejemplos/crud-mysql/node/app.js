69ec7dd24473b234de0ecd26208672c0d66a225c
César <cesarlpb@gmail.com>
2023-06-15 09:44:02 +0200

    Añadimos lista de endpoints y colores de consola

--- node/ejemplos/crud-mysql/node/app.js
+++ node/ejemplos/crud-mysql/node/app.js
@@ -1,28 +1,41 @@
 const http = require('http');
 
+let endpoints = ["albumes", "artistas", "posts", "tracks", "usuarios"]
+// Códigos de color para consola:
+const red = "\x1b[31m";
+const green = "\x1b[32m";
+const yellow = "\x1b[33m";
+
 http.createServer(function (req, res) {
-  console.log("Consola del servidor")
-  // TODO: colocar un switch para manejar las rutas
-  if(req.url == "/albumes"){
+  let esEndpointValido = endpoints.includes(req.url.replaceAll("/", "")) // true si es endpoint de endpoints
+  let url = req.url
+  let endpoint = req.url.replaceAll("/", "")
+  if(esEndpointValido){
+    console.log(green, `200. URL: ${url} endpoint: ${endpoint}`)
     res.writeHead(200, { 'Content-Type': 'text/html; charset = UTF-8' })
-    res.write("<h1>Albumes</h1>")
+    res.write(`<h1>${endpoint}</h1>`)
     res.end()
-  }else{
+  }else if(req.url == "/"){
+    console.log(green, `200. URL: ${url} lista de endpoints`)
     res.writeHead(200, { 'Content-Type': 'text/html; charset = UTF-8' })
     let html = generarHTML()
     res.write(html)
     res.end() 
+  }else{
+    console.error(red, `404. URL: ${url} no encontrada`)
+    res.writeHead(404, { 'Content-Type': 'text/html; charset = UTF-8' })
+    res.write("<h1>Error 404: No encontrado</h1>")
+    res.end()
   }
 }).listen(8080, () => { console.log("Servidor en ejecución en http://localhost:8080") })
 
 function generarHTML() {
-  let arr = ["albumes", "artistas", "posts", "tracks", "usuarios"]
   let baseUrl = "http://localhost:8080"
   let html = ""
   html += "<h1>Endpoints</h1>"
-  for(let i = 0; i<arr.length; i++){
-    let enlace = baseUrl + "/" + arr[i] 
-    html += `<a href=${enlace} target="_blank">${arr[i]}</a><br>`
+  for(let i = 0; i<endpoints.length; i++){
+    let enlace = baseUrl + "/" + endpoints[i] 
+    html += `<a href=${enlace} target="_blank">${endpoints[i]}</a><br>`
   }
   return html
 }
