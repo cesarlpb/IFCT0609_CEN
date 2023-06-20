541415c5d5a1a0feb9c71845be209bc3cf431e52
César P <cesarlpb@gmail.com>
2023-06-20 12:36:58 +0200

    Node - Añadimos dotenv y resolvemos conexión a db

--- ubuntu/server.js
+++ ubuntu/server.js
@@ -1,19 +1,23 @@
 const mysql = require("mysql")
 const http = require("http")
+require('dotenv').config() // Accedemos al .env
 
 const con = mysql.createConnection({
-  host : "localhost",
-  user : "root",
-  password : "contraseña", // <-- colocamos contraseña
-  database : "nombre_db" // <-- cambiad nombre db 
+  host : "127.0.0.1", // con localhost en el servidor no conecta, más info: https://github.com/nodejs/help/issues/1555 y https://stackoverflow.com/questions/30266221/node-js-mysql-error-connect-econnrefused
+  port: 3306, // (opcional) Usamos puerto por defecto
+  user : process.env.USER,
+  password : process.env.PASSWORD, // <-- colocamos contraseña
+  database : process.env.DATABASE, // <-- cambiad nombre db 
+  // socketPath: '/var/run/mysqld/mysqld.sock'
 });
-
+const db = process.env.DATABASE;
+const table = "temp" // <--- cambiad nombre tabla
 // Queries
-const selectAll = "SELECT * FROM nombre_tabla LIMIT 10;"; // <--- cambiad nombre tabla
+const selectAll = `SELECT * FROM ${table} LIMIT 10;`; 
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Conexión exitosa a la base de datos " + "nombre_tabla");
+  console.log("Conexión exitosa a la base de datos " + db);
   http.createServer(function(req, res){
     let id = req.url.split("/")[2]
     if(req.url.startsWith("/datos") && !id && req.method == "GET"){
@@ -27,5 +31,5 @@ con.connect(function(err) {
       res.writeHead(400)
       res.end("Bad Request")
     }
-  }).listen(8080, () => { console.log("http://localhost:8080")});
+  }).listen(8080, () => { console.log("http://localhost:8080/datos")});
 });
