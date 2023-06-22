8f2dfa47f143529a9b83a3ab9802ce8cc7287432
César <cesarlpb@gmail.com>
2023-06-22 13:48:44 +0200

    deleteOne

--- node/express/app.js
+++ node/express/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 // Importamos clase Nota
 const { Nota } = require("./nota");
 // Importamos las funciones desde functions.js
-const { getAll, getOne, createOne, updateOne } = require("./functions");
+const { getAll, getOne, createOne, updateOne, deleteOne } = require("./functions");
 
 // Creamos una instancia de express llamada app
 const app = express();
@@ -22,7 +22,7 @@ app.set('json spaces', 2)
 // GET de un array (lista)
 app.get("/notas", (req, res, next) => {
   // Sembramos 3 datos en el array de notas
-  if(Nota.notas.length < 3) Nota.seed();
+  if(Nota.notas.length == 0) Nota.seed();
   let notas = getAll(); // llamamos a la función getAll de functions para recibir el arr de notas
   res.json(notas);
 });
@@ -31,24 +31,22 @@ app.get("/notas/:id", (req, res, next) => {
   let nota = getOne(req.params.id); // pasamos el id a la función getOne de functions para recibir una nota
   res.json(nota)
 });
+// POST de un elemento
 app.post("/notas", (req, res, next) => {
   let nota = req.body; // objeto de tipo Nota
   let nuevaNota = createOne(nota);
   res.json(nuevaNota)
 });
+// PUT de un elemento
 app.put("/notas/:id", (req, res, next) => {
   let notaEditada = updateOne(req.params.id, req.body);
   res.json(notaEditada)
 });
-app.delete("/notas/:id", (req, res, next) => {
-  res.json({"mensaje": `Nota ${req.params.id} borrada`})
-});
-
-// TODO: creamos los endpoints para CRUD de un arr
-// GET de un elemento
-// POST de un elemento
-// PUT de un elemento
 // DELETE de un elemento
+app.delete("/notas/:id", (req, res, next) => {
+  let mensaje = deleteOne(req.params.id);
+  res.json(mensaje)
+});
 
  // Aplicación que sale por el puerto 8080
 app.listen(8080, () => {
--- node/express/functions.js
+++ node/express/functions.js
@@ -4,7 +4,7 @@ const { notas, Nota } = require("./nota"); // Con las llaves, es un array // SIN
 // getAll -> devuelve el array de notas
 function getAll() {  
   // Sembramos 3 datos en el array de notas
-  if(Nota.notas.length < 3) Nota.seed();
+  if(Nota.notas.length == 0) Nota.seed();
   // TODO: opciones de filtrado
   return notas;
 }
@@ -14,7 +14,7 @@ function getOne(id) {
 }
 // createOne -> crea una nota, recibe un objeto nota y lo devuelve
 function createOne(nota) {
-  if(Nota.notas.length < 3) Nota.seed(); // Sembramos 3 datos en el array de notas
+  if(Nota.notas.length == 0) Nota.seed(); // Sembramos 3 datos en el array de notas
   let nuevaNota = new Nota(
     nota?.id || Nota.notas.length + 1, 
     nota?.titulo, 
@@ -30,7 +30,7 @@ function createOne(nota) {
   };
 }
 function updateOne(id, nota) {
-  if(Nota.notas.length < 3) Nota.seed(); // Sembramos 3 datos en el array de notas
+  if(Nota.notas.length == 0) Nota.seed(); // Sembramos 3 datos en el array de notas
   let notaEditada = new Nota(
     Number.parseInt(id), 
     nota?.titulo, 
@@ -45,8 +45,17 @@ function updateOne(id, nota) {
     return {"mensaje": `No se ha podido editar la nota con id ${id}`}
   };
 }
+function deleteOne(id){
+  if(id <= Nota.notas.length){
+    Nota.notas.splice(id-1, 1);
+    return {"mensaje": `Nota ${id} borrada`}
+  }else{
+    return{"mensaje": `No se ha podido borrar la nota con id ${id}`}
+  }
+}
 
 exports.getAll = getAll;
 exports.getOne = getOne;
 exports.createOne = createOne;
 exports.updateOne = updateOne;
+exports.deleteOne = deleteOne;
