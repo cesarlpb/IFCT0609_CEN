e9f375bb5374888fe078ab11594843dbc476c4ad
César <cesarlpb@gmail.com>
2023-06-22 10:21:01 +0200

    Rename de PostIt a Nota

--- node/express/app.js
+++ node/express/app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const app = express();
 app.set('json spaces', 2) // Configuración para que los JSON aparezcan indentados
 
-class PostIt{
-  constructor(titulo, texto, prioridad, fecha){
+class Nota{
+  constructor(id, titulo, texto, prioridad, fecha){
+    this.id = id;
     this.titulo = titulo;
     this.texto = texto;
     this.prioridad = prioridad;
@@ -13,15 +14,15 @@ class PostIt{
   }
 }
 // Datos para los endpoints
-const postIts = [
-  new PostIt("Comprar pan", "Comprar pan en el supermercado", "", "2023-06-23"),
-  new PostIt("Comprar leche", "Comprar leche en el supermercado", "normal", "2023-06-23"),
-  new PostIt("Comprar huevos", "Comprar huevos en el supermercado", "urgente", "2023-10-22"),
+const notas = [
+  new Nota(1, "Comprar pan", "Comprar pan en el supermercado", "", "2023-06-23"),
+  new Nota(2, "Comprar leche", "Comprar leche en el supermercado", "normal", "2023-06-23"),
+  new Nota(3, "Comprar huevos", "Comprar huevos en el supermercado", "urgente", "2023-10-22"),
 ];
 
 // GET de un array (lista)
-app.get("/postits", (req, res, next) => {
-  res.json(postIts);
+app.get("/notas", (req, res, next) => {
+  res.json(notas);
  });
 
 // TODO: creamos los endpoints para CRUD de un arr
