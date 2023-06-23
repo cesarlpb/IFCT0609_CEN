79bf51a3cc9f23aec3f61c80931835cefd9a877a
César <cesarlpb@gmail.com>
2023-06-23 11:58:57 +0200

    Ejemplo de param id en ruta

--- node/express/2.tutorial/router.js
+++ node/express/2.tutorial/router.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const notas = [
+  {id: 1, texto: "Nota 1"},
+  {id: 2, texto: "Nota 2"},
+  {id: 3, texto: "Nota 3"}
+]
+
 router.all('/', function(req, res){
   let fechaHora = new Date().toLocaleString();
   console.log(fechaHora + " - Request all en /")
@@ -15,6 +21,17 @@ router.post('/hola', function(req, res){
   let fechaHora = new Date().toLocaleString();
   console.log(fechaHora + " - Request POST en /hola")
   res.send("Esta es la respuesta a un POST");
+});
+router.get('/notas', function(req, res){
+  let fechaHora = new Date().toLocaleString();
+  console.log(fechaHora + " - Request GET en /notas")
+  res.send(notas);
+});
+router.get('/notas/:id', function(req, res){
+  let fechaHora = new Date().toLocaleString();
+  console.log(fechaHora + " - Request GET en /notas/" + req.params.id)
+  let nota = notas.find(nota => nota.id == req.params.id);
+  res.send(nota);
 });
 
 module.exports = router;
