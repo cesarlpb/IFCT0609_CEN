0b9c4c75269c46d4afdee23ead3b402ac3b78133
César <cesarlpb@gmail.com>
2023-06-23 12:08:23 +0200

    Ejemplo de parámetro de URL opcional en ruta

--- node/express/2.tutorial/router.js
+++ node/express/2.tutorial/router.js
@@ -13,10 +13,15 @@ router.all('/', function(req, res){
   console.log(fechaHora + " - Request all en /")
   res.send("Hola mundo desde express");
 });
-router.get('/hola', function(req, res){
+// El ? después del parámetro name indica que es opcional, entonces podemos usar el endpoint en caso de recibirlo o no
+router.get('/hola/:name?', function(req, res){
   let fechaHora = new Date().toLocaleString();
   console.log(fechaHora + " - Request GET en /hola")
-  res.send("Hola");
+  if(req.params.name){
+    res.send("Hola, " + req.params.name + "!");
+  }else{
+    res.send("Hola!")
+  }
 });
 router.post('/hola', function(req, res){
   let fechaHora = new Date().toLocaleString();
@@ -34,6 +39,7 @@ router.get('/notas/:id', function(req, res){
   let nota = notas.find(nota => nota.id == req.params.id);
   res.send(nota);
 });
+// TODO: Ejercicio. Crear un solo enpoint que use un parámetro opcional para filtrar por nombre y por id, es decir, "fusionar" los dos endpoints /:id y /:id/:name
 router.get('/notas/:id/:name', function(req, res){
   let fechaHora = new Date().toLocaleString();
   console.log(fechaHora + " - Request GET en /notas/" + req.params.id)
