c315ce256cf0b0cdbe0e3546392b12f3db728310
César <cesarlpb@gmail.com>
2023-06-23 10:51:35 +0200

    Añadimos /hola ruta GET y POST

--- node/express/2.tutorial/app.js
+++ node/express/2.tutorial/app.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-   res.send("Hola mundo desde express");
+  let fechaHora = new Date().toLocaleString();
+  console.log(fechaHora + " - Request GET en /")
+  res.send("Hola mundo desde express");
+});
+app.get('/hola', function(req, res){
+  let fechaHora = new Date().toLocaleString();
+  console.log(fechaHora + " - Request GET en /hola")
+  res.send("Hola");
+});
+app.post('/hola', function(req, res){
+  let fechaHora = new Date().toLocaleString();
+  console.log(fechaHora + " - Request POST en /hola")
+  res.send("Esta es la respuesta a un POST");
 });
 
-app.listen(3000);
+app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
