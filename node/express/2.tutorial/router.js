76d33fec1f1b92462524a312633c0b6253902600
César <cesarlpb@gmail.com>
2023-06-26 11:49:45 +0200

    Form para crear usuario -> GET y POST

--- node/express/2.tutorial/router.js
+++ node/express/2.tutorial/router.js
@@ -16,9 +16,9 @@ const notas = [
   {id: 2, texto: "Nota 2", name: "María"},
 ]
 const users = [
-  {id: "000", name: "Pepe"},
-  {id: "001", name: "María"},
-  {id: "002", name: "Juan"},
+  {id: 0, name: "Pepe", email: "pepe@gmail.com"},
+  {id: 1, name: "María", email: "maria@gmail.com"},
+  {id: 2, name: "Juan", email: "juan@gmail.com"},
 ]
 // Middleware - console.log de la hora, la ruta, la ip y el método de cada request
 // Middleware #1
@@ -65,14 +65,37 @@ router.post('/hola', function(req, res){
 // Formulario para crear un usuario
 router.get('/form', function(req, res){
   // Formulario para crear un usuario
-  // TODO: crear el formulario
-  res.send('<h2>Formulario</h2>')
+  let form = `
+    <head>
+      <meta charset="UTF-8">
+      <title>Formulario para crear un usuario</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    </head>
+    <body>
+      <div class="container col-6 py-3">
+      <h1>Formulario para crear un usuario</h1>
+      <form action="/form" method="POST">
+      <input type="text" class="form-control" name="name" placeholder="Nombre de usuario">
+      <br>
+      <input type="text" class="form-control" name="email" placeholder="Email">
+      <br>
+      <input type="submit" class="btn btn-primary" value="Enviar">
+      </form>
+      </div>
+    </body>
+    `
+  res.send(form)
 });
 router.post('/form', function(req, res){
   // Recibimos el form y lo enviamos de vuelta
-  // TODO: crear el usuario en el array users
+  let nuevoUsuario = req.body; // req.body es el objeto que enviamos en el body del request al hacer un POST
+  let nuevoId = users.length;
+  nuevoUsuario.id = nuevoId; // 3, 4, 5...
+  let nuevoUsuarioIdx = users.push(nuevoUsuario); // Actualizamos variable con el dato DESDE el array y lo devolvemos para comprobar que se añadió con todos los campos
+  console.log(users[nuevoUsuarioIdx - 1])
   // TODO: crear el usuario en la base de datos
-  res.send(users); // o el user nuevo
+  res.status(201); // 201 Created
+  res.send(users[nuevoUsuarioIdx - 1]); // o el user nuevo
 });
 // Final de formulario para crear un usuario
 router.get('/notas', function(req, res){
@@ -94,7 +117,7 @@ router.get('/notas/:id/:name', function(req, res){
 router.get("/users", function(req, res){
   res.send(users)
 });
-router.get('/users/:id([0-9]{3})', function(req, res){
+router.get('/users/:id([0-9]{1})', function(req, res){
   let user = users.find(user => user.id == req.params.id);
   res.send(user);
 });
