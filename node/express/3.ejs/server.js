74e6424482659c7cbab56bcc98d129119561c357
César <cesarlpb@gmail.com>
2023-06-28 13:08:13 +0200

    Proyecto 3. EJS - Código inicial

---
+++ node/express/3.ejs/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.send("/")
+  // res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.send("/about")
+  // res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('Servidor en ejecución en http://localhost:8080/');
