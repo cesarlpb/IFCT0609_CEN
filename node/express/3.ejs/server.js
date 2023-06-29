d8825394cc962ad307f1a7df89f00d4c27d68875
César <cesarlpb@gmail.com>
2023-06-29 09:43:32 +0200

    Pasamos variables a la vista desde server.js

--- node/express/3.ejs/server.js
+++ node/express/3.ejs/server.js
@@ -8,7 +8,16 @@ app.set('view engine', 'ejs');
 
 // index page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  const mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  const tagline = "No programming concept is complete without a cute animal mascot.";
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
 
 // about page
