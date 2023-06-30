1a01082baf944ec76aa77d8ff62e92a37e6cb95a
César <cesarlpb@gmail.com>
2023-06-30 10:22:28 +0200

    Bootstrap y estilos básicos para las vistas

--- node/express/4.forms-ejs/app.js
+++ node/express/4.forms-ejs/app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.use("/css", express.static('public'));
+
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // enlaces a register y login
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/index', {title: 'Inicio'});
 });
 
 // Formulario de registro
 app.get('/register', function(req, res) {
-  res.render('pages/register');
+  res.render('pages/register', {title: 'Registro'});
 });
 
 // Formulario de registro
 app.get('/login', function(req, res) {
-  res.render('pages/login');
+  res.render('pages/login', {title: 'Login'});
 });
 
 app.listen(8080);
