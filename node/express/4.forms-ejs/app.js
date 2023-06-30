d219161e5372822393b1210c96749dd89fe6ec69
César <cesarlpb@gmail.com>
2023-06-30 10:40:17 +0200

    Forms y responses para comprobar datos de POST

--- node/express/4.forms-ejs/app.js
+++ node/express/4.forms-ejs/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.use("/css", express.static('public'));
 
 // set the view engine to ejs
@@ -11,14 +15,24 @@ app.get('/', function(req, res) {
   res.render('pages/index', {title: 'Inicio'});
 });
 
-// Formulario de registro
+// Formulario de registro - GET
 app.get('/register', function(req, res) {
   res.render('pages/register', {title: 'Registro'});
 });
 
-// Formulario de registro
+// Formulario de login - GET
 app.get('/login', function(req, res) {
   res.render('pages/login', {title: 'Login'});
+});
+
+// Processado de los forms
+app.post('/register', function(req, res) {
+  // Añadir el usuario a la base de datos
+  res.send(req.body);
+});
+app.post('/login', function(req, res) {
+  // Comprobar el usuario en la base de datos
+  res.send(req.body);
 });
 
 app.listen(8080);
