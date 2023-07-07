116e62c48e9c305334c93c4da205977bcc527022
César <cesarlpb@gmail.com>
2023-07-07 12:04:10 +0200

    Enpoint de GET de lista de posts en /posts

--- examenes/server.js
+++ examenes/server.js
@@ -1,11 +1,18 @@
 const http = require('http');
 
-const posts = []
+const posts = [
+  {"id": 1, "title": "Post 1 de ejemplo", "content": "Contenido del post 1", tags: "ejemplo, post, api"},
+]
 
 http.createServer((req, res) => {
   // TODO:
-  // GET /posts o /api/posts - devuelve una lista de posts
-  // POST /posts o /api/posts - crea un nuevo post
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola Mundo');
+  // GET /posts   - devuelve una lista de posts
+  // POST /posts  - crea un nuevo post
+  if(req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<h2>Endpoints: GET en "/posts" y POST en "/posts"</h2>');
+  }else if(req.method === 'GET' && req.url === '/posts') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(posts, null, 2));
+  }
 }).listen(3000, () => {console.log("Servidor en ejecución en http://localhost:3000")});
